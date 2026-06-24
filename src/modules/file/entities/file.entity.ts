25ae391cc4049f850d0c8bafd16c049dcebfad49
coderThanh <nguyenvanthanh6499@gmail.com>
2026-06-24 16:41:33 +0700

    feat(file): add createdByUser relation field with serialization
    add required imports for class-transformer and UserRelation type

--- src/modules/file/entities/file.entity.ts
+++ src/modules/file/entities/file.entity.ts
@@ -1,6 +1,12 @@
+import { Expose, Type } from 'class-transformer';
+
 import { Prisma } from '@prisma/client';
+import { UserRelation } from '@/modules/user/entities/user.entity';
 
 export class FileEntity {
+  @Expose()
+  createdByUser: UserRelation | null;
+
   //
   public static selectRelation: Prisma.FileSelect = {
     id: true,
